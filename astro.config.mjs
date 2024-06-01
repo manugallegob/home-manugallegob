650a9d5b691f0c0a791fe7e4db7b23fceb4a96f9
manugallegob <manuelgallegobarra@gmail.com>
2024-06-01 12:40:10 +0200

    ajuste modo dark por defecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://manugallegob.com",
+  // site: "https://manugallegob.com",
 });
